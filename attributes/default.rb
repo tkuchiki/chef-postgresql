e4a65b23ac1a70065f4ce007d9ef8d013543e6c3
tkuchiki <kuchiki.taku@gmail.com>
2014-07-31 09:43:24 +0900

    Fixed initdb

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,6 +4,7 @@ if node[:platform] == "centos"
     :dev_package    => "postgresql92-postgresql-devel",
     :server_package => "postgresql92-postgresql-server",
     :service_name   => "postgresql92-postgresql",
+    :pgdir          => "/opt/rh/postgresql92/root/var/lib/pgsql",
   }
 elsif node[:platform] == "amazon"
   default[:postgresql] = {
@@ -11,7 +12,10 @@ elsif node[:platform] == "amazon"
     :dev_package    => "postgresql9-devel",
     :server_package => "postgresql9-server",
     :service_name   => "postgresql",
+    :pgdir          => "/var/lib/pgsql9",
   }
 end
+
+default[:postgresql][:pgdata] = node[:postgresql][:pgdir] + "/data"
 
 default[:postgresql][:packages] = []
--- recipes/server.rb
+++ recipes/server.rb
@@ -2,12 +2,13 @@ include_recipe "chef-postgresql::centos" if node[:platform] == "centos"
 
 service_cmd  = "/sbin/service"
 service_name = node[:postgresql][:service_name]
+pgdata       = node[:postgresql][:pgdata]
 
 package node[:postgresql][:server_package]
 
 bash "postgresql initdb" do
   code   "#{service_cmd} #{service_name} initdb"
-  not_if "#{service_cmd} #{service_name} status"
+  not_if { File.exists?(pgdata + "/PG_VERSION") && File.exists?(pgdata + "/base") }
 end
 
 service service_name do
